bc98301a1dc8113c9aad0ad75a2e12c7e9da630c
Michael A Trindade <michael.atrindade@gmail.com>
2024-02-26 20:10:55 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,5 @@
+const estudante = 'Caroline';   // Variavel constante - sempre precisa atribuir valor
+let professora;                 // Variavel normal - pode criar variavel sem atribuir valor
+professora = 'Ana';
+
+console.log(estudante, professora);
