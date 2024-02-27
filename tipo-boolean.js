6838665cafe37890b7fb4e0f7e758dd4b91bc591
Michael A Trindade <michael.atrindade@gmail.com>
2024-02-26 22:29:21 -0300

    tipo boolean

---
+++ tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if (estaAprovado === true) {        // === sempre retorna true ou false
+    console.log('parabéns, boas festas');
+} else {
+    console.log('REPROVADO, boas festas');
+}
+
+if (estudante === 'Fernando') {     // === sempre retorna true ou false
+    console.log(`olá, ${estudante}`);
+} else {
+    console.log('quem é vc');
+}
