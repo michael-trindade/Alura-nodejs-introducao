6aebd8f6dade664efe2de324c3aa6d8895c387f0
Michael A Trindade <michael.atrindade@gmail.com>
2024-02-26 22:08:01 -0300

    tipo string

---
+++ tipo-string.js
@@ -0,0 +1,14 @@
+const estudante = "Caroline";
+const docente = 'Ana';
+const cumprimento = "Nosso lema é 'estudar bastante!'";
+const citacao = `Ju diz: "nosso lema é 'estudar bastante!'"`;
+
+console.log(cumprimento);
+console.log(citacao);
+
+// template string
+console.log(`a estudante chama ${estudante}`);
+
+const senha = 'SenhaSegura123' + estudante.toUpperCase();
+console.log(senha);
+
