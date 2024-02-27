2c3f4f8e62854bcc4950db23ed3c3cd1ab399bd8
Michael A Trindade <michael.atrindade@gmail.com>
2024-02-26 22:10:51 -0300

    tipo string v2

--- tipo-string.js
+++ tipo-string.js
@@ -12,3 +12,12 @@ console.log(`a estudante chama ${estudante}`);
 const senha = 'SenhaSegura123' + estudante.toUpperCase();
 console.log(senha);
 
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
