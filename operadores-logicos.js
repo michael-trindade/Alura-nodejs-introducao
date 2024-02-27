dac168a2dad1e2df219de3bf40b0c0fa178880d0
Michael A Trindade <michael.atrindade@gmail.com>
2024-02-27 20:44:52 -0300

    operador de negação

--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,9 +1,16 @@
 const notaFinal = 6;
-const faltas = 5;
+const faltas = 2;
+const advertencias = 0;
 
 //if (notaFinal < 7 || faltas > 4) {        // || OR -> operador lógico
 if (notaFinal < 7 && faltas > 4) {          // && AND -> operador lógico
     console.log('reprovado, boas festas');
 } else {
     console.log('não foi reprovado por falta');
+}
+
+if (faltas >= 2 && !advertencias) {  // ! -> operador negação, neste caso nega false e retorna true
+    console.log('recebeu bônus');
+} else {
+    console.log('não recebeu bônus');
 }
