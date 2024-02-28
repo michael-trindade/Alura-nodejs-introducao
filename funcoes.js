f080ed510d8a259a180035af21ace8f850d1f6f2
Michael A Trindade <michael.atrindade@gmail.com>
2024-02-27 22:28:55 -0300

    observação das funções

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,10 @@
 // parâmetros/argumentos
 // retorno
+console.log(exibeInfosEstudante('Caroline', 10)); // Pode executar antes da função
+console.log(exibeInfosEstudante('Ana', 7));
 
-function exibeInfosEstudante(nome, nota) {
+function exibeInfosEstudante(nome, nota) {      // Declaração de Função
     return `o nome é ${nome} e a nota é ${nota}`;
 }
 
-console.log(exibeInfosEstudante('Caroline', 10));
-console.log(exibeInfosEstudante('Ana', 7));
+// hoisting - içar
